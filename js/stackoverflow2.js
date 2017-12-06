892d7847fc01604706afe782d0aa13b53b3bda5b
Aman Gaur <aman.gaur1011@gmail.com>
2017-12-07 01:49:56 +0530

    asked the question on stack overflow

--- js/stackoverflow2.js
+++ js/stackoverflow2.js
@@ -2,9 +2,7 @@ var ViewModel = function() {
 	this.currentCat = ko.observable( new Cat() );
 	console.log(this.currentCat().clickCount())
 	this.incrementCount = function(){
-		console.log(this.currentCat().clickCount())
 		this.currentCat().clickCount(this.currentCat().clickCount() + 1);
-		console.log(this.currentCat().clickCount())
 	};
 }
 
@@ -12,7 +10,7 @@ var Cat = function() {
 	this.clickCount = ko.observable(0);
 	this.name = ko.observable('Tabby');
 	this.imgSrc = ko.observable('tabby.jpg');
-	this.imgAttribution = ko.observable('Aman');
+	this.imgAttribution = ko.observable('XXXX');
 
 	this.nickNames = ko.observableArray(
 		[
@@ -22,25 +20,5 @@ var Cat = function() {
 			{name: 'Tabitha Tab Tabby'}
 		]
 	);
-	this.level = ko.computed(function(){
-		if (this.clickCount() <=10) {
-			return "Infant";
-		}
-		else if (this.clickCount() <=20) {
-			return "Baby";
-		}
-		else if (this.clickCount() <=30) {
-			return "Kid";
-		}
-		else if (this.clickCount() <=40) {
-			return "Teen";
-		}
-		else if (this.clickCount() <=50) {
-			return "Adult";
-		}
-		else if (this.clickCount() >50) {
-			return "Old";
-		}
-	}, this);
 }
 ko.applyBindings(new ViewModel())
