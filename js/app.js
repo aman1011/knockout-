95bc0177f90d8956c61beaa876fb9caba1a94371
Aman Gaur <aman.gaur1011@gmail.com>
2017-12-07 01:20:31 +0530

    added the currentCat 'with'

--- js/app.js
+++ js/app.js
@@ -1,15 +1,27 @@
 var ViewModel = function() {
+	this.currentCat = ko.observable( new Cat() );
+	console.log(this.currentCat().clickCount())
+	this.incrementCount = function(){
+		this.currentCat().clickCount(this.currentCat().clickCount() + 1);
+	};
+}
+
+var Cat = function() {
 	this.clickCount = ko.observable(0);
 	this.name = ko.observable('Tabby');
 	this.imgSrc = ko.observable('tabby.jpg');
 	this.imgAttribution = ko.observable('Aman');
 
-	this.incrementCount = function(){
-		this.clickCount(this.clickCount() + 1);
-	};
+	this.nickNames = ko.observableArray(
+		[
+			{name: 'Tabtab'},
+			{name: 'T-bone'},
+			{name: 'Mr. T'},
+			{name: 'Tabitha Tab Tabby'}
+		]
+	);
 	this.level = ko.computed(function(){
 		if (this.clickCount() <=10) {
-			console.log('reched in if');
 			return "Infant";
 		}
 		else if (this.clickCount() <=20) {
